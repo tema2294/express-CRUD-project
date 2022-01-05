bced518bad9e8b2d4009e130cfaaf20a44c6d250
tema2294 <tema.marinin@mail.ru>
2022-01-05 17:07:11 +0300

    commit #4

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ const DB_URL = 'mongodb+srv://tema2294:943833fF@cluster0.hq7ki.mongodb.net/myFir
 app.use(express.json())
 app.use('/auth',authRouter)
 
-const corsOptions ={
-    origin:['http://localhost:3000'],
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-app.use(cors());
-app.options('*', cors(corsOptions));
+// const corsOptions ={
+//     origin:['http://localhost:3000'],
+//     credentials:true,            //access-control-allow-credentials:true
+//     optionSuccessStatus:200
+// }
+// app.use(cors());
+// app.options('*', cors(corsOptions));
 
 async function startApp() {
     try {
