ecf8f6c758a92193e0dbf25b4d5b2eb11d494a30
tema2294 <tema.marinin@mail.ru>
2022-01-07 01:10:19 +0300

    commit #5

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -7,13 +7,12 @@ export function authMiddleware (req, res, next) {
     }
 
     try {
-        const token = req.headers.authorization
+        const token = req.headers.authorization.split(' ')[1]
         if (!token) {
             return res.status(403).json({message: "Пользователь не авторизован"})
         }
-        const decodedData = jwt.verify(token, secret)
-        req.user = decodedData
-
+        req.user = jwt.verify(token, secret)
+        next();
     } catch (e) {
         console.log(e)
         return res.status(403).json({message: "Пользователь не авторизован"})
